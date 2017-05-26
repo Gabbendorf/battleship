5ff6420cc21094ad7dbd81eda399472a6879343a
Gabriella <gabriellamds@yahoo.it>
2017-05-26 12:37:54 +0100

    implemented invite_to_choose_ship and ships with tests

--- lib/ui.rb
+++ lib/ui.rb
@@ -9,10 +9,6 @@
       @stdout.puts "Welcome to Battleship. Are you ready to play?"
     end
 
-    def invite_to_place_ship
-      @stdout.puts "Player 1: place the first ship."
-    end
-
     def display_grid
       grid = "  "
       (1..10).each {|column| grid << column.to_s + "  "}
@@ -20,6 +16,14 @@
       dots = ".  " * 10
       ("A ".."J " ).each {|raw| grid << raw + dots + "\n"}
       @stdout.puts grid
+    end
+
+    def invite_to_choose_ship
+      @stdout.puts "Player 1: choose a ship to place."
+    end
+
+    def ships
+      @stdout.puts "- Submarine\n- Destroyer\n- Cruiser\n- Aircraft-carrier."
     end
 
   end
--- spec/ui_spec.rb
+++ spec/ui_spec.rb
@@ -18,10 +18,16 @@ RSpec.describe Ui do
     expect(output.string).to include("  1  2  3  4  5  6  7  8  9  10  \nA .  .  .  .  .  .  .  .  .  .  \nB .  .  .  .  .  .  .  .  .  .  \nC .  .  .  .  .  .  .  .  .  .  \nD .  .  .  .  .  .  .  .  .  .  \nE .  .  .  .  .  .  .  .  .  .  \nF .  .  .  .  .  .  .  .  .  .  \nG .  .  .  .  .  .  .  .  .  .  \nH .  .  .  .  .  .  .  .  .  .  \nI .  .  .  .  .  .  .  .  .  .  \nJ")
   end
 
-  it "invites player 1 to place first ship" do
-    ui.invite_to_place_ship
+  it "invites player 1 to choose ship to place" do
+    ui.invite_to_choose_ship
 
-    expect(output.string).to include("Player 1: place the first ship.")
+    expect(output.string).to include("Player 1: choose a ship to place.")
+  end
+
+  it "displays list of ships" do
+    ui.ships
+
+    expect(output.string).to include ("- Submarine\n- Destroyer\n- Cruiser\n- Aircraft-carrier.")
   end
 
 end
