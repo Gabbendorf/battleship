86ecf8e3c2f0a74363da0455620053b66fdfe6cc
Gabriella <gabriellamds@yahoo.it>
2017-05-28 18:00:46 +0100

    deleted commented-out cell and cells? and their tests

--- lib/grid.rb
+++ lib/grid.rb
@@ -6,28 +6,6 @@ class Grid
     @ships_placed = {}
   end
 
-  # COLUMNS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-  #
-  # ROWS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-
-  # def cells
-  #   cells = []
-  #   COLUMNS.each do |number|
-  #     cell = ""
-  #     ROWS.each do |letter|
-  #       cell += number.to_s
-  #       cell += letter
-  #       cells.push(cell)
-  #       cell = ""
-  #     end
-  #   end
-  #   cells
-  # end
-  #
-  # def cell?(position)
-  #   cells.include?(position)
-  # end
-
   def place_ship(x,y,ship)
     if ship.length == 1
       @ships_placed[ship] = x,y
