7a5a2f820dfdc2b43b41ce59e79ae6e854e0e372
Gabriella <gabriellamds@yahoo.it>
2017-05-30 14:01:29 +0100

    deleted delete_sunk_ship and test

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -7,65 +7,54 @@ RSpec.describe Grid do
 
   describe "registers positions of all ships placed" do
     it "registers submarine with its position in the list of ships placed" do
-      submarine = Ship.new("submarine",1)
+      submarine = Ship.new("submarine", 1)
 
-      grid.place_ship(5,5,submarine)
+      grid.place_ship(5, 5, submarine)
 
-      expect(grid.ships_placed).to eq({submarine => [[5,5]]})
+      expect(grid.ships_placed).to eq({submarine => [[5, 5]]})
     end
 
     it "registers destroyer with its position in the list of ships placed" do
-      destroyer = Ship.new("destroyer",2)
+      destroyer = Ship.new("destroyer", 2)
 
-      grid.place_ship(5,5,destroyer)
+      grid.place_ship(5, 5, destroyer)
 
-      expect(grid.ships_placed).to eq({destroyer => [[5,5], [6,5]]})
+      expect(grid.ships_placed).to eq({destroyer => [[5, 5], [6, 5]]})
     end
 
     it "registers cruiser with its position in the list of ships placed" do
-      cruiser = Ship.new("cruiser",3)
+      cruiser = Ship.new("cruiser", 3)
 
-      grid.place_ship(5,5,cruiser)
+      grid.place_ship(5, 5, cruiser)
 
-      expect(grid.ships_placed).to eq({cruiser => [[5,5], [6,5],[7,5]]})
+      expect(grid.ships_placed).to eq({cruiser => [[5, 5], [6, 5],[7, 5]]})
     end
 
     it "registers aircraft_carrier with its position in the list of ships placed" do
-      aircraft_carrier = Ship.new("aircraft-carrier",4)
+      aircraft_carrier = Ship.new("aircraft-carrier", 4)
 
-      grid.place_ship(5,5,aircraft_carrier)
+      grid.place_ship(5, 5, aircraft_carrier)
 
-      expect(grid.ships_placed).to eq({aircraft_carrier => [[5,5], [6,5],[7,5],[8,5]]})
+      expect(grid.ships_placed).to eq({aircraft_carrier => [[5, 5], [6, 5],[7, 5],[8, 5]]})
     end
   end
 
   describe "checks if a cell is already occupied by a ship" do
     it "returns true if a cell is occupied" do
-      aircraft_carrier = Ship.new("aircraft-carrier",4)
+      aircraft_carrier = Ship.new("aircraft-carrier", 4)
 
-      grid.place_ship(2,2,aircraft_carrier)
+      grid.place_ship(2, 2, aircraft_carrier)
 
-      expect(grid.ship?([4,2])).to eq(true)
+      expect(grid.ship?([4, 2])).to eq(true)
     end
 
     it "returns false if a cell is not occupied" do
-      aircraft_carrier = Ship.new("aircraft-carrier",4)
+      aircraft_carrier = Ship.new("aircraft-carrier", 4)
 
-      grid.place_ship(2,2,aircraft_carrier)
+      grid.place_ship(2, 2, aircraft_carrier)
 
-      expect(grid.ship?([9,2])).to eq(false)
+      expect(grid.ship?([9, 2])).to eq(false)
     end
-  end
-
-  it "deletes ship from list of ships placed if this sinks" do
-    submarine = Ship.new("submarine",1)
-    grid.place_ship(5,5,submarine)
-
-    game.attack([5,5],submarine)
-    submarine.register_cells_hit([5,5],grid)
-    grid.delete_sunk_ship(submarine)
-
-    expect(grid.ships_placed.length).to eq(0)
   end
 
 end
