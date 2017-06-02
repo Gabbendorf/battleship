8e1db440be80dab041baeab2142d896edaf422d6
Gabriella <gabriellamds@yahoo.it>
2017-06-02 14:02:30 +0100

    implemented vertical_cells in Orientation w test

--- lib/orientation.rb
+++ lib/orientation.rb
@@ -9,4 +9,15 @@ class Orientation
     occupied_cells
   end
 
+  def vertical_cells(x,y, ship_length)
+    occupied_cells = [[x,y]]
+    letters_after = (y.."Z").map {|letter| letter}
+    index = 0
+    (ship_length-1).times do |index|
+      next_letter = letters_after[index+1]
+      occupied_cells.push([x,next_letter])
+    end
+    occupied_cells
+  end
+
 end
