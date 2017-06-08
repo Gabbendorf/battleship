f6c43dfd6efba7866916a150a4b31c07cffd7e37
Gabriella <gabriellamds@yahoo.it>
2017-06-08 13:17:54 +0100

    implemented validate_ship_to_place_position

--- lib/validations.rb
+++ lib/validations.rb
@@ -8,4 +8,21 @@ class Validations
     end
   end
 
+  def validate_ship_to_place_position(position)
+    splitted_position = position.split(",")
+    if !valid_numbers_for_columns.include?(splitted_position[0].to_i) || !valid_letters_for_rows.include?(splitted_position[1]) || splitted_position[3] != "v" || splitted_position[3] != "h"
+      :error
+    end
+  end
+
+  private
+
+  def valid_numbers_for_columns
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  end
+
+  def valid_letters_for_rows
+    ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+  end
+
 end
--- spec/validations_spec.rb
+++ spec/validations_spec.rb
@@ -1,19 +1,60 @@
+require 'spec_helper'
 require_relative '../lib/validations'
 
 RSpec.describe Validations do
 
   let(:validations) {Validations.new}
 
-  it "raises error for wrong input for ship number" do
-    output = validations.validate_ship_number("5")
+  describe "checks player's entered number corresponds to a ship" do
+    it "returns :error for invalid number" do
+      output = validations.validate_ship_number("5")
 
-    expect(output).to eq(:error)
+      expect(output).to eq(:error)
+    end
+
+    it "returns :error for invalid input type" do
+      output = validations.validate_ship_number("hello")
+
+      expect(output).to eq(:error)
+    end
   end
 
-  it "raises error for wrong input for ship number" do
-    output = validations.validate_ship_number("hello")
+  describe "checks player's inputs for ship to place are all valid" do
+    it "returns :error if 1st coordinate is not number" do
+      output = validations.validate_ship_to_place_position("a, b, h")
 
-    expect(output).to eq(:error)
+      expect(output).to eq(:error)
+    end
+
+    it "returns :error if 1st coordinate is not valid number" do
+      output = validations.validate_ship_to_place_position("100, b, h")
+
+      expect(output).to eq(:error)
+    end
+
+    it "returns :error if 2nd coordinate is a number" do
+      output = validations.validate_ship_to_place_position("1, 2, h")
+
+      expect(output).to eq(:error)
+    end
+
+    it "returns :error if 2nd coordinate is an invalid letter" do
+      output = validations.validate_ship_to_place_position("1, x, h")
+
+      expect(output).to eq(:error)
+    end
+
+    it "returns :error if 3rd coordinate (orientation) is not v (vertical case)" do
+      output = validations.validate_ship_to_place_position("1, b, c")
+
+      expect(output).to eq(:error)
+    end
+
+    it "returns :error if 3rd coordinate (orientation) is not h (horizontal case)" do
+      output = validations.validate_ship_to_place_position("1, b, d")
+
+      expect(output).to eq(:error)
+    end
   end
 
 end
