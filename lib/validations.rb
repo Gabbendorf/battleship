d21e146e480f983824c2657a1c6556f4ebf5f779
Gabriella <gabriellamds@yahoo.it>
2017-06-08 10:55:11 +0100

    deleted dependancy to Ui

--- lib/validations.rb
+++ lib/validations.rb
@@ -1,9 +1,5 @@
 class Validations
 
-  def initialize(ui)
-    @ui = ui
-  end
-
   def validate_ship_number(number)
     number = number.to_i
     valid_numbers = [1, 2, 3, 4]
--- spec/validations_spec.rb
+++ spec/validations_spec.rb
@@ -2,18 +2,16 @@ require_relative '../lib/validations'
 
 RSpec.describe Validations do
 
-  let(:output) {StringIO.new}
-  let(:input) {StringIO.new}
-  let(:grid_display) {GridDisplay.new}
-  let(:ui) {Ui.new(input, output, grid_display)}
-  let(:validations) {Validations.new(ui)}
+  let(:validations) {Validations.new}
 
-  it "it raises error for wrong input for ship number" do
-    input = StringIO.new("2\n")
-    ui = Ui.new(input, output, grid_display)
-    validations = Validations.new(ui)
-
+  it "raises error for wrong input for ship number" do
     output = validations.validate_ship_number("5")
+
+    expect(output).to eq(:error)
+  end
+
+  it "raises error for wrong input for ship number" do
+    output = validations.validate_ship_number("hello")
 
     expect(output).to eq(:error)
   end
