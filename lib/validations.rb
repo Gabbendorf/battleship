6eef7e5bd8f2397e0491f7dd7d3e215298042b5c
Gabriella <gabriellamds@yahoo.it>
2017-06-08 13:36:05 +0100

    refactored validate_ship_to_place_position

--- lib/validations.rb
+++ lib/validations.rb
@@ -10,19 +10,27 @@ class Validations
 
   def validate_ship_to_place_position(position)
     splitted_position = position.split(",")
-    if !valid_numbers_for_columns.include?(splitted_position[0].to_i) || !valid_letters_for_rows.include?(splitted_position[1]) || splitted_position[3] != "v" || splitted_position[3] != "h"
+    if !valid_coordinates_and_orientation?(splitted_position)
       :error
     end
   end
 
   private
 
-  def valid_numbers_for_columns
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  def valid_coordinates_and_orientation?(position)
+    valid_number?(position) && valid_letter?(position) && valid_orientation?(position)
   end
 
-  def valid_letters_for_rows
-    ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+  def valid_orientation?(position)
+    position[2] = "h" || position[2] = "v"
+  end
+
+  def valid_number?(position)
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].include?(position[0].to_i)
+  end
+
+  def valid_letter?(position)
+    ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"].include?(position[1])
   end
 
 end
