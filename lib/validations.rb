f839875ec5fabc836d9e0c8a61f1fe76f4c42b69
Gabriella <gabriellamds@yahoo.it>
2017-06-08 15:32:40 +0100

    implemented validate_ship_position_on_grid w/ tests

--- lib/validations.rb
+++ lib/validations.rb
@@ -15,14 +15,28 @@ class Validations
     end
   end
 
+  def validate_ship_position_on_grid(ship, position)
+    splitted_position = position.split(",")
+    if splitted_position[2] == "v" && number_for[splitted_position[1]] < ship.length || splitted_position[2] == "h" && (splitted_position[0].to_i + ship.length-1) > 10
+      :error
+    end
+  end
+
   private
+
+  def number_for
+    {"a" => 1,
+     "b" => 2,
+     "c" => 3,
+    }
+  end
 
   def valid_coordinates_and_orientation?(position)
     valid_number?(position) && valid_letter?(position) && valid_orientation?(position)
   end
 
   def valid_orientation?(position)
-    position[2] = "h" || position[2] = "v"
+    position[2] == "h" || position[2] == "v"
   end
 
   def valid_number?(position)
--- spec/validations_spec.rb
+++ spec/validations_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/validations'
+require_relative '../lib/ship'
 
 RSpec.describe Validations do
 
@@ -20,39 +21,95 @@ RSpec.describe Validations do
 
   describe "checks player's inputs for ship to place are all valid" do
     it "returns :error if 1st coordinate is not number" do
-      output = validations.validate_ship_to_place_position("a, b, h")
+      output = validations.validate_ship_to_place_position("a,b,h")
 
       expect(output).to eq(:error)
     end
 
     it "returns :error if 1st coordinate is not valid number" do
-      output = validations.validate_ship_to_place_position("100, b, h")
+      output = validations.validate_ship_to_place_position("100,b,h")
 
       expect(output).to eq(:error)
     end
 
     it "returns :error if 2nd coordinate is a number" do
-      output = validations.validate_ship_to_place_position("1, 2, h")
+      output = validations.validate_ship_to_place_position("1,2,h")
 
       expect(output).to eq(:error)
     end
 
     it "returns :error if 2nd coordinate is an invalid letter" do
-      output = validations.validate_ship_to_place_position("1, x, h")
+      output = validations.validate_ship_to_place_position("1,x,h")
 
       expect(output).to eq(:error)
     end
 
     it "returns :error if 3rd coordinate (orientation) is not v (vertical case)" do
-      output = validations.validate_ship_to_place_position("1, b, c")
+      output = validations.validate_ship_to_place_position("1,b,c")
 
       expect(output).to eq(:error)
     end
 
     it "returns :error if 3rd coordinate (orientation) is not h (horizontal case)" do
-      output = validations.validate_ship_to_place_position("1, b, d")
+      output = validations.validate_ship_to_place_position("1,b,d")
 
       expect(output).to eq(:error)
+    end
+  end
+
+  describe "checks if ship stays inside grid according to player's input for its position" do
+    it "returns :error if ship doesn't stay inside grid vertically" do
+      destroyer = Ship.new("destroyer", 2)
+      grid_position = "1,a,v"
+
+      output = validations.validate_ship_position_on_grid(destroyer, grid_position)
+
+      expect(output).to eq(:error)
+    end
+
+    it "returns :error if ship doesn't stay inside grid vertically" do
+      destroyer = Ship.new("destroyer", 2)
+      grid_position = "6,a,v"
+
+      output = validations.validate_ship_position_on_grid(destroyer, grid_position)
+
+      expect(output).to eq(:error)
+    end
+
+    it "returns nil if ship stays inside grid vertically" do
+      destroyer = Ship.new("destroyer", 2)
+      grid_position = "2,b,v"
+
+      output = validations.validate_ship_position_on_grid(destroyer, grid_position)
+
+      expect(output).to eq(nil)
+    end
+
+    it "returns :error if ship doesn't stay inside grid horizontally" do
+      destroyer = Ship.new("destroyer", 2)
+      grid_position = "10,a,h"
+
+      output = validations.validate_ship_position_on_grid(destroyer, grid_position)
+
+      expect(output).to eq(:error)
+    end
+
+    it "returns :error if ship doesn't stay inside grid horizontally" do
+      destroyer = Ship.new("destroyer", 2)
+      grid_position = "10,j,h"
+
+      output = validations.validate_ship_position_on_grid(destroyer, grid_position)
+
+      expect(output).to eq(:error)
+    end
+
+    it "returns nil if ship stays inside grid horizontally" do
+      destroyer = Ship.new("destroyer", 2)
+      grid_position = "9,j,h"
+
+      output = validations.validate_ship_position_on_grid(destroyer, grid_position)
+
+      expect(output).to eq(nil)
     end
   end
 
