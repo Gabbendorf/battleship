fa556fe33330ad79605be28894b1ab37cd131406
Gabriella <gabriellamds@yahoo.it>
2017-06-13 14:36:23 +0100

    implemented select_random_position in Computer)

--- lib/computer.rb
+++ lib/computer.rb
@@ -9,8 +9,15 @@ class Computer
     @ships_list.ships.keys.sample
   end
 
+  def select_random_position
+    position = []
+    position.push((1..10).map {|n| n}.sample)
+    position.push(("A".."J").map {|n| n}.sample)
+    position.push([:vertical, :horizontal].sample)
+    position
+  end
+
   # def place_ship(ship)
   #   @grid.mark_ship_positions
   # end
-
 end
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -8,9 +8,24 @@ RSpec.describe Computer do
   let(:computer) {Computer.new(grid, ships_list)}
 
   it "chooses ship from ships list randomly until list is empty" do
-    ship_name = computer.select_ship
+    ship_name = computer.select_randomly_ship
 
     expect(ships_list.ships.keys.include?(ship_name)).to eq(true)
+  end
+
+  it "gives position for ship placement randomly" do
+    position = computer.select_random_position
+
+    x = position[0]
+    y = position[1]
+    orientation = position[2]
+    valid_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    valid_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+
+    expect(position.length).to eq(3)
+    expect(valid_numbers).to include(x)
+    expect(valid_letters).to include(y)
+    expect([:vertical, :horizontal]).to include(orientation)
   end
 
   xit "places submarine on grid" do
