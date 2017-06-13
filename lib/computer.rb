b841a3ea83b39031400288d30c4b58baded8dde0
Gabriella <gabriellamds@yahoo.it>
2017-06-13 16:41:15 +0100

    implemented computer.place_ship without validations yet

--- lib/computer.rb
+++ lib/computer.rb
@@ -3,6 +3,17 @@ class Computer
   def initialize(grid, ships_list)
     @grid = grid
     @ships_list = ships_list
+    @create_ship = CreateShip.new
+  end
+
+  def place_ship(x, y, ship, orientation)
+    while @ships_list.ships.size > 0
+      random_ship_name = select_randomly_ship
+      position = select_random_position
+      ship = @create_ship.ship_from_name(random_ship_name)
+      @grid.mark_ship_positions(position[0], position[1], ship, position[2])
+      @ships_list.delete_selected_ship(random_ship_name)
+    end
   end
 
   def select_randomly_ship
@@ -11,13 +22,10 @@ class Computer
 
   def select_random_position
     position = []
-    position.push((1..10).map {|n| n}.sample)
-    position.push(("A".."J").map {|n| n}.sample)
+    position.push((1..10).map {|number| number}.sample)
+    position.push(("A".."J").map {|letter| letter}.sample)
     position.push([:vertical, :horizontal].sample)
     position
   end
 
-  # def place_ship(ship)
-  #   @grid.mark_ship_positions
-  # end
 end
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -28,8 +28,14 @@ RSpec.describe Computer do
     expect([:vertical, :horizontal]).to include(orientation)
   end
 
-  xit "places submarine on grid" do
-    computer.place_ship("submarine")
+  # how can I test this further?
+  it "places random ship on grid from list of 6 ships" do
+    destroyer = Ship.new("destroyer", 2)
+    possible_ship_types = ["submarine", "destroyer", "cruiser", "aircraft-carrier"]
+
+    computer.place_ship(1, "A", destroyer, :vertical)
+
+    expect(grid.ships_placed.size).to eq(6)
   end
 
 end
