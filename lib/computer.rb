7c2f6f8bcc630701e8290e9b1b19f3a7f0ea587b
Gabriella <gabriellamds@yahoo.it>
2017-06-13 21:34:38 +0100

    added validations into computer.place_ship

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,20 +1,32 @@
 class Computer
 
-  def initialize(grid, ships_list)
+  def initialize(grid, ships_list, validations)
     @grid = grid
     @ships_list = ships_list
+    @validations = validations
     @create_ship = CreateShip.new
   end
 
   def place_ship(x, y, ship, orientation)
     while @ships_list.ships.size > 0
       random_ship = select_randomly_ship
-      position = select_random_position
-      @grid.mark_ship_positions(position[0], position[1], random_ship, position[2])
+      position = valid_position(random_ship, select_random_position)
+      @grid.mark_ship_positions(position[:x], position[:y], random_ship, position[:orientation])
     end
   end
 
   private
+
+  def valid_position(ship, position)
+    validation_result = @validations.validate_position_for_ship(ship, position)
+    while validation_result != :valid_ship_position
+      if validation_result == :invalid_ship_position || validation_result == :invalid_placement
+        position = select_random_position
+      end
+      validation_result = @validations.validate_position_for_ship(ship, position)
+    end
+    position
+  end
 
   def select_randomly_ship
     random_ship_name = @ships_list.ships.keys.sample
@@ -24,10 +36,10 @@ class Computer
   end
 
   def select_random_position
-    position = []
-    position.push(rand(1..10))
-    position.push(("A".."J").map {|letter| letter}.sample)
-    position.push([:vertical, :horizontal].sample)
+    position = {}
+    position[:x] = rand(1..10)
+    position[:y] = (("A".."J").map {|letter| letter}.sample)
+    position[:orientation] = ([:vertical, :horizontal].sample)
     position
   end
 
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -5,16 +5,17 @@ RSpec.describe Computer do
 
   let(:grid) {Grid.new}
   let(:ships_list) {ShipsList.new}
-  let(:computer) {Computer.new(grid, ships_list)}
+  let(:validations) {Validations.new}
+  let(:computer) {Computer.new(grid, ships_list, validations)}
 
   # how can I test this further?
   it "places random ship on grid from list of 6 ships" do
     destroyer = Ship.new("destroyer", 2)
-    # possible_ship_types = ["submarine", "destroyer", "cruiser", "aircraft-carrier"]
 
     computer.place_ship(1, "A", destroyer, :vertical)
 
     expect(grid.ships_placed.size).to eq(6)
+    puts grid.ships_placed
   end
 
 end
